6dff3e041476c455df736b56ca4904ca3429dc33
Butrint Babuni <butrint.xh.babuni@gmail.com>
2023-11-19 23:28:59 +0100

    Fix

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -437,7 +437,7 @@
         <div class="col-xxl-3 col-md-6 box-col-6 col-ed-none wow zoomIn">
         <div class="card purchase-card"><img class="img-fluid" src="{{ asset('assets/images/dashboard/purchase.png') }}" alt="vector mens with leptop">
             <div class="card-body pt-3"> 
-            <h6 class="mb-3">Buy <a href="#">Pro Account </a>to Explore Primium Features</h6><a class="purchase-btn btn btn-primary btn-hover-effect f-w-500" href="https://1.envato.market/3GVzd" target="_blank">Purchase Now</a>
+            <h6 class="mb-3">Buy <a href="#">Pro Account </a>to Explore Primium Features</h6><a class="purchase-btn btn btn-primary btn-hover-effect f-w-500" href="#" target="_blank">Purchase Now</a>
             </div>
         </div>
         </div>
